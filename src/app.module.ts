4b4cf286acbd30aabe995d83bfdeffee6e443159
deneve <denevee@proton.me>
2024-08-17 10:58:05 +0300

    init redis & database modules & services

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UrlModule } from './url/url.module';
+import { RedisModule } from './redis/redis.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [UrlModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60,
+        limit: 10,
+      },
+    ]),
+    UrlModule,
+    RedisModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
